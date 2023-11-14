454cb24f41097dd56ed75612eb09bf99967dcdfb
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-11-14 18:59:13 -0300

    Adição de classe para listar usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Sidne\TesourariaBaba\App\Controller\CadastrarContribuintesController;
+use Sidne\TesourariaBaba\App\Controller\ListarContribuintesController;
 
 require '../vendor/autoload.php';
 
@@ -10,4 +11,9 @@ if($q == "cadastrarContribuinte") {
 
     $cad = new CadastrarContribuintesController;
     $cad->cadastroContribuinte($_REQUEST);
+}
+else if($q == "listarContribuintes") {
+
+    $cad = new ListarContribuintesController;
+    $cad->listagemContribuintes($_REQUEST);
 }
